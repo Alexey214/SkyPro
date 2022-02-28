d41a2e825e212c15b7467d38ce830a8ecd61ba2e
brain <golenev214@gmail.com>
2022-02-28 17:32:41 +0300

    изм. текст

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_*-*_");
+	System.out.println("Hello World");
     }
 }
